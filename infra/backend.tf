76711af3a74c07b99c8ea7cf7d761ef92c237906
Bruno Lopes <brunclopes_lab@outlook.com>
2024-06-15 01:49:32 -0300

    Ajustando storage account do backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "adlsterraformstateyxz"
+    storage_account_name = "adlsterraformstatebxo"
     container_name       = "terraform-state"
     key                  = "workspace_modules/infra/terraform.tfstate"
   }
