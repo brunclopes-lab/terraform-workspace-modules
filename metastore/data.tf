39364e6ebd75a23a49ac2000846c3d097698fc65
Bruno Lopes <brunclopes_lab@outlook.com>
2024-06-16 19:26:06 -0300

    Removendo variaveis das pipelines

--- metastore/data.tf
+++ metastore/data.tf
@@ -5,10 +5,12 @@ data "azuread_service_principal" "sp_azuredatabricks" {
 
 # OBTENDO O ID DO DATABRICKS REFERENTE AO SERVICE PRINCIPAL
 data "databricks_service_principal" "sp_terraform" {
+  provider = databricks.azure_account
   application_id = "e2ec2faa-d292-4c56-a9fd-3665914268d6"
 }
 
 # OBTENDO O ID DO USUARIO 
 data "databricks_user" "principal_user" {
+  provider = databricks.azure_account
   user_name = "brunclopes_lab_outlook.com#ext#@brunclopeslaboutlook.onmicrosoft.com"
 }
