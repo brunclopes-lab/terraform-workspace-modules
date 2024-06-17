8143577170aa964b7914ec04e272e4628670074e
Bruno Lopes <brunclopes_lab@outlook.com>
2024-06-17 12:15:21 -0300

    Removendo outputs

--- metastore/output.tf
+++ metastore/output.tf
@@ -2,10 +2,10 @@ output "databricks_metastore_id" {
   value = module.databricks_metastore.databricks_metastore_id
 }
 
-output "databricks_sp_terraform_id" {
-  value = data.databricks_service_principal.sp_terraform.id
-}
+# output "databricks_sp_terraform_id" {
+#   value = data.databricks_service_principal.sp_terraform.id
+# }
 
-output "databricks_user_id" {
-  value = data.databricks_user.principal_user.id
-}
+# output "databricks_user_id" {
+#   value = data.databricks_user.principal_user.id
+# }
